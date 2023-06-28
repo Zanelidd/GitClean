e215df896253fcf3f9229525b0e9d3718c5aa1f0
Damien Arnaud <113262967+Zanelidd@users.noreply.github.com>
2023-06-28 22:45:46 +0200

    Login oK

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { CurrentUserProvider } from "./contexts/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <CurrentUserProvider>
+      <App />
+    </CurrentUserProvider>
   </React.StrictMode>
 );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import UserContext from "../contexts/UserContext";
+
 export default function Home() {
+  const { user } = useContext(UserContext);
+  console.info(user);
   return (
     <div className="">
-      <h1>Home</h1>
+      <h1>{user.firstname}</h1>
       <p>Home</p>
     </div>
   );
