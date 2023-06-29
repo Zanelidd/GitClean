3ddcbc9983cf2528914c5aded5d109980b77ef6b
Frederico Cassola <cfcassola@gmail.com>
2023-06-29 10:54:17 +0200

    Add fetch to itemsList and map

--- frontend/src/components/ItemsList.jsx
+++ frontend/src/components/ItemsList.jsx
@@ -1,49 +1,26 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function ItemsList() {
-  // eslint-disable-next-line no-unused-vars
   const [itemsListData, setItemsListData] = useState([]);
 
-  useEffect(() =>
-    // axios.get()
-    {}, []);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/database`)
+      .then((response) => setItemsListData(response.data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <>
-      <tr>
-        <td>10</td>
-        <td>IOS</td>
-        <td>6</td>
-        <td>4</td>
-        <td>128</td>
-      </tr>
-      <tr>
-        <td>10</td>
-        <td>IOS</td>
-        <td>6</td>
-        <td>4</td>
-        <td>128</td>
-      </tr>
-      <tr>
-        <td>10</td>
-        <td>IOS</td>
-        <td>6</td>
-        <td>4</td>
-        <td>128</td>
-      </tr>
-      <tr>
-        <td>10</td>
-        <td>IOS</td>
-        <td>6</td>
-        <td>4</td>
-        <td>128</td>
-      </tr>
-      <tr>
-        <td>10</td>
-        <td>IOS</td>
-        <td>6</td>
-        <td>4</td>
-        <td>128</td>
-      </tr>
+      {itemsListData.map((item) => (
+        <tr key={item.id}>
+          <td>{item.model}</td>
+          <td>{item.os_id}</td>
+          <td>{item.screen_size}</td>
+          <td>{item.network_id}</td>
+          <td>128</td>
+        </tr>
+      ))}
     </>
   );
 }
