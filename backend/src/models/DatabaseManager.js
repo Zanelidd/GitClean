47a2b10ee6b34125be33482369317b9d2d58219c
Frederico Cassola <cfcassola@gmail.com>
2023-06-29 12:21:58 +0200

    Add query find phones with all columns in DatabaseManager.js

--- backend/src/models/DatabaseManager.js
+++ backend/src/models/DatabaseManager.js
@@ -6,7 +6,15 @@ class DatabaseManager extends AbstractManager {
   }
 
   findAllWithAllColumns() {
-    return this.database.query(`SELECT * FROM ${this.table}`);
+    return this.database
+      .query(`select b.name brand, p.model, p.screen_size, n.name network, o.version os_version, r.name ram, s.name storage from ${this.table} p
+    inner join brand b on b.id = p.brand_id
+    inner join network n ON n.id = p.network_id
+    inner join os o ON o.id = p.os_id
+    inner join product_ram pr on pr.product_id = p.id
+    inner join ram r on r.id = pr.ram_id
+    inner join product_storage ps on ps.product_id = p.id
+    inner join storage s on s.id = ps.storage_id;`);
   }
 }
 
